68b85107fa5e1a7287fe99f9c6c5a651e8c840e6
Hello_Haru_System <halfdansc@gmail.com>
2024-12-10 19:49:21 +0100

    added the main game thread to the game panel

--- src/main/java/UserInterface/GamePanel.java
+++ src/main/java/UserInterface/GamePanel.java
@@ -3,7 +3,7 @@ package UserInterface;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // screen settings
     final int originalTileSize = 16; // 16x16 tile
@@ -17,9 +17,22 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenColumn; // 769 pixels
     final int screenHeight = tileSize * maxScreenRow;   // 576 pixels
 
+    // Main game Thread
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        this.gameThread = new Thread(this);
+        this.gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        // Main game loop
     }
 }
